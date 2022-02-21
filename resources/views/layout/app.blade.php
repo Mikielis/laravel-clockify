c40b7c560b64a9cfe1c21ae8b4d3c2de938bc4db
Kamil Mikielis <kamil.mikielis@gmail.com>
2022-02-21 23:28:14 +0100

    Add early version of main layout and assets - main CSS and JS files

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="index,follow"/>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name') }}</title>
+
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/layout.min.css') }}" media="all">
+
+        <script>
+            window.vars = window.vars || {};
+            window.vars['csrf_token'] = '{{ csrf_token() }}';
+        </script>
+    </head>
+    <body>
+        <div class="content">
+            @yield('content')
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+
+        <?php if (App::environment('local')): ?>
+            <script src="{{ asset('js/jq.main.js') }}"></script>
+        <?php else: ?>
+            <script src="{{ asset('js/jq.main.min.js') }}"></script>
+        <?php endif; ?>
+
+        @stack('scripts')
+    </body>
+</html>

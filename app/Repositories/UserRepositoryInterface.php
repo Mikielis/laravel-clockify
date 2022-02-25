988c8f22afdafc22063110d5d240136eb46affe3
Kamil Mikielis <kamil.mikielis@gmail.com>
2022-02-25 10:55:25 +0100

    Change attribute in UserRepositoryInterface method

--- app/Repositories/UserRepositoryInterface.php
+++ app/Repositories/UserRepositoryInterface.php
@@ -5,7 +5,7 @@ use App\Models\User;
 
 interface UserRepositoryInterface
 {
-    public function getGoogleUser(string $socialId): User|NULL;
+    public function getGoogleUser(string $email): User|NULL;
 
     public function addGoogleUser(string $name, string $email, string $socialId): User|NULL;
 }

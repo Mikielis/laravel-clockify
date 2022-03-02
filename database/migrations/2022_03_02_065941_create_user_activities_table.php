9a9460b657b8de7212bad5577e0e70689f5dc04d
Kamil Mikielis <kamil.mikielis@gmail.com>
2022-03-02 09:05:09 +0100

    Create DB migration with user_activities table

---
+++ database/migrations/2022_03_02_065941_create_user_activities_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('type', 30)->index('type');
+            $table->string('page', 255);
+            $table->string('activity_description', 400);
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onUpdate('restrict')
+                ->onDelete('restrict');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_activities');
+    }
+};

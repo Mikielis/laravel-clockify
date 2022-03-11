9d957d98d8cf909186b9abde455d3a9613e5d0eb
Kamil Mikielis <kamil.mikielis@gmail.com>
2022-03-11 22:48:05 +0100

    Set SoftDeletes to Client, Project, Timesheet and User models

--- app/Models/Timesheet.php
+++ app/Models/Timesheet.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use App\Models\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Timesheet extends Model
 {
-    use HasFactory, Uuid;
+    use HasFactory, Uuid, SoftDeletes;
 
     protected $table = 'timesheet';
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles, Uuid;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles, Uuid, SoftDeletes;
 
     protected string $guard = 'user';
 
